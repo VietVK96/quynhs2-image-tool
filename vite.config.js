5572e48f8fd60a255faffc1d4925d2ea8c1cdf4c
vietvk <vietvk@ltsgroup.tech>
2026-05-05 23:14:54 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/quynhs2-image-tool/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
